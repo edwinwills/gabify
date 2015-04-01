f99053179a4ab23fad332880c867a9817155babd
Edwin Wills <edwin@rentify.com>
2015-04-01 16:24:04 +0100

    Initial commit

---
+++ add_gab.js
@@ -0,0 +1,5 @@
+var img = document.createElement("img");
+img.setAttribute('src', 'https://s3-eu-west-1.amazonaws.com/uploads-eu.hipchat.com/37086/289123/mFk252cZamJGbG0/gabriele-asset.png')
+img.setAttribute('style', 'position: fixed; bottom: 0px; z-index: 99999;')
+
+document.body.appendChild(img);
